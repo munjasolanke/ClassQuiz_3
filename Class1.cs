a1447fb4c1b4c0d61074e210cef4ddd05573e9e7
munjasolanke <54603212+munjasolanke@users.noreply.github.com>
2019-09-24 22:25:16 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassQuiz_3
+{
+    public class Class1 : Program
+    {
+        public string FamilyNumber;
+        public string FamilyAdvisor;
+
+        /// <summary>
+        /// Default constructor attempt 1
+        /// </summary>
+        public Class1()
+        {
+            FamilyNumber = "U123";
+            //slastName = "Solanke";
+        }
+        //
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassQuiz_3
+{
+    public class Program
+    {
+        public string firstName;
+        public string lastName;
+
+        // static variables
+        public static string salutationMr = "Mr";
+        string getName()
+        {
+            return firstName + " " + lastName;
+        }
+        public Program()
+        {
+            firstName = "Munja";
+            lastName = "Solanke";
+        }
+        public Program(string lname)
+        {
+            lastName = lname;
+        }
+        static void Main(string[] args)
+        {
+            Program Class2 = new Program();
+        }
+
+    }
+
+}
